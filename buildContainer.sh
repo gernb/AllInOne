4b38c35a2cab179c31395830736b4b2b3f23a5ad
peter bohac <gern@1dot0.net>
2025-07-21 19:37:21 -0700

    specify the linux triple

--- buildContainer.sh
+++ buildContainer.sh
@@ -4,6 +4,6 @@ REPOSITORY=registry:5000
 
 set -ex
 #swift package --swift-sdk $SDK -c $CONFIGURATION build-container-image --allow-insecure-http both --repository ${REPOSITORY}/app-server --from swift:slim --product server
-swift build --swift-sdk $SDK -c $CONFIGURATION --product server
+swift build --swift-sdk $SDK --triple aarch64-swift-linux-musl -c $CONFIGURATION --product server
 swift run containertool --allow-insecure-http both --from swift:slim --resources ./public --repository ${REPOSITORY}/app-server ./.build/aarch64-swift-linux-musl/${CONFIGURATION}/server
 
